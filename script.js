a765cf20b9f5c774b342816e1066ab4a984b9ed6
Shilpa-Selvakumar <118339506+Shilpa-Selvakumar@users.noreply.github.com>
2025-07-13 16:58:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const books = [
   {
     title: "Diary of a Wimpy Kid- Hard Luck",
     author: "Jeff Kinney",
-    image: "/images/salty.jpg",
+    image: "images/Hardluck.jpg",
     description: "A powerful novel about racial injustice in the Deep South."
   },
   {
